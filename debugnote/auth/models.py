1372858255995ff56c19ab4de826210b38c79c53
jihun <wlgns1501@gmail.com>
2023-03-24 11:31:10 +0900

    auth

--- debugnote/auth/models.py
+++ debugnote/auth/models.py
@@ -1,14 +1,10 @@
 from django.db import models
 
-
 # Create your models here.
 class User(models.Model):
-    email: models.CharField(unique=True, null=False, blank=False, max_length=200)
-    password: models.CharField(null=False, blank=False, max_length=100)
+    email: models.CharField(max_length=200)
+    password: models.CharField(max_length=100)
     createdAt: models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.email
-
-
-    # def get_user(self) :
--- debugnote/auth/views.py
+++ debugnote/auth/views.py
@@ -8,7 +8,6 @@ from .serializers import BaseUserSeiralizer
 from rest_framework.views import APIView
 from rest_framework import permissions, status
 from drf_yasg.utils import swagger_auto_schema
-from .open_api_params import signup_params
 
 
 class UserView(APIView):
